094d1b9472468b1c1019621b44fc7f09f22deb36
Bryce <brycekane5@gmail.com>
2021-04-15 15:16:40 -0400

    Added writing to json and polished funcitonality

--- calculator.go
+++ calculator.go
@@ -6,50 +6,60 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
 )
 
 type Weight struct {
-	Weight []int `json:"weight"`
+	Weight float64
+	Date   string
 }
 
 func main() {
-	readweight()
-	readingjson()
+	weight := readweight()
+	readingjson(weight)
 }
 func discardBuffer(r *bufio.Reader) {
 	r.Discard(r.Buffered())
 }
-func readingjson() {
-	weightarr := readJSON("weight.json")
-	fmt.Print("Weights")
+func readingjson(weightt float64) {
+	weightarr := readJSON("weight.json", weightt)
+	//fmt.Print("Weights")
 	var weightavg float64
 	for _, weight := range weightarr {
-		var x float64 = float64(weight)
+		var x float64 = float64(weight.Weight)
 		weightavg += x
+		fmt.Print(x, ",")
 	}
 	weightavg = weightavg / float64(len(weightarr))
+	fmt.Println()
 	fmt.Printf("Average weight %g", weightavg)
+	fmt.Println()
+	fmt.Printf("You are %g pounds down from your original weight", weightarr[0].Weight-weightarr[len(weightarr)-1].Weight)
 }
-func readJSON(fileName string) []int {
+func readJSON(fileName string, i float64) []Weight {
 	jsonFile, err := os.Open("weight.json")
-	// if we os.Open returns an error then handle it
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened weight.json")
-	// defer the closing of our jsonFile so that we can parse it later on
+
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	//var result map[string]interface{}
-	var weights Weight
+
+	var weights []Weight
+
 	json.Unmarshal(byteValue, &weights)
-	fmt.Println(weights.Weight[0])
-	return weights.Weight
+
+	dt := time.Now()
+	weights = append(weights, Weight{Weight: i, Date: dt.Format("04-014-2021")})
+	file, _ := json.MarshalIndent(weights, "", "")
+	_ = ioutil.WriteFile("weight.json", file, 0644)
+	return weights
 
 }
-func readweight() {
+func readweight() float64 {
 	stdin := bufio.NewReader(os.Stdin)
-	var i int
+	var i float64
 	_, err := fmt.Fscanln(stdin, &i)
 	if err != nil {
 		//throw error, loop
@@ -58,5 +68,7 @@ func readweight() {
 		readweight()
 	} else {
 		fmt.Print("read number", i, "from stdin")
+		return i
 	}
+	return i
 }
